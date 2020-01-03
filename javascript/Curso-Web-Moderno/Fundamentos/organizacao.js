d5672978a5c16e2f7ecfce692a2a952877df487c
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2020-01-03 07:28:38 -0300

    Criação do projeto

---
+++ javascript/Curso-Web-Moderno/Fundamentos/organizacao.js
@@ -0,0 +1,10 @@
+console.log("Sentença de código")
+// Sentenças de códigos estruturados finalizados ou não com ";"
+
+
+{
+    {
+       console.log("Olá");
+       console.log("Mundo!") // Padrão do curso 
+    }
+}
---
+++ javascript/Curso-Web-Moderno/variaveisEconstantes.js
@@ -0,0 +1,24 @@
+/*
+* Por regra geral recomenda-se utilizar o let 
+* para a criação de variáveis
+*/
+
+var a = 3
+let b = 4
+
+var a = 30
+b = 40
+
+console.log(a, b)
+
+a = 300
+b = 400
+
+console.log(a, b)
+
+const c = 5
+/* 
+* Não se consegue alterar constantes 
+* c = 50 acarretaria erro.
+*/
+console.log(c)
