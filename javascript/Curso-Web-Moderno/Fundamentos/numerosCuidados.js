45db2cf6ded2d575fb76a70392ebe6dcea091aae
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2020-01-03 07:54:29 -0300

    Adicionando estudo sobre números em Javascript

---
+++ javascript/Curso-Web-Moderno/Fundamentos/numerosCuidados.js
@@ -0,0 +1,17 @@
+// Causará resultado infinito
+console.log(7/0)
+
+/*
+* O Javascript receberá a String e converterá
+* se houver um valor numérico identificado pelo 
+* Javascript
+*/
+console.log("10" / 2)
+
+// Não entendendo acarretará NaN (Not a number)
+console.log("Show!" * 2)
+
+console.log(0.1 + 0.7)
+
+// console.log(10.toString())
+console.log((10.345).toFixed(2))
