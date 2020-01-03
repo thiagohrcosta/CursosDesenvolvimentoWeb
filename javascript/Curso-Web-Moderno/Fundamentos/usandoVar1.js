6fa5510ec25043e99e604e927300bb372d0a1a41
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2020-01-03 11:18:03 -0300

    Entendendo o uso de Var e Let no JavaScript

---
+++ javascript/Curso-Web-Moderno/Fundamentos/usandoVar1.js
@@ -0,0 +1,19 @@
+{ 
+    { 
+        { 
+            { 
+                var sera = "Será???" 
+                console.log(sera)
+            } 
+        } 
+    } 
+}
+console.log(sera)
+
+function teste(){
+    var local = 123
+    console.log(local)
+}
+
+teste()
+//  console.log(local)  // não será visível fora do escopo da varíavel.
