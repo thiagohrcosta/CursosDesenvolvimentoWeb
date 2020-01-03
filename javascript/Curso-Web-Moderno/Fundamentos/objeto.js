f1ebcbad58368b093b7b5c64adf7f51052935ef4
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2020-01-03 09:18:35 -0300

    Conceitos fundamentais de objeto

---
+++ javascript/Curso-Web-Moderno/Fundamentos/objeto.js
@@ -0,0 +1,13 @@
+// Objetos são criados com chaves {}
+const prod1 = {}
+prod1.nome = 'Celular Ultra Mega'
+prod1.price = 4998.90
+prod1['desconto'] = 0.40
+console.log(prod1)
+
+const prod2 = {
+    nome: 'Camisa Polo',
+    preço: 79.90
+}
+
+console.log(prod2)
