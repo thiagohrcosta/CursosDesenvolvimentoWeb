213495178bcca4573146181f4e1a4c1301cb19f8
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2020-01-03 07:38:13 -0300

    Adicionando estudo sobre tipagem fraca

---
+++ javascript/Curso-Web-Moderno/Fundamentos/tipagemFraca.js
@@ -0,0 +1,15 @@
+let qualquer = 'Legal'
+console.log(qualquer)
+
+console.log(typeof qualquer)
+
+qualquer = 3.1516
+console.log(qualquer)
+console.log(typeof qualquer)
+
+/* Evitar nomes genéricos e siglas 
+* para variáveis e constantes tais
+* como dispostos abaixo.
+*/
+let valor = ''
+let numero = 1
