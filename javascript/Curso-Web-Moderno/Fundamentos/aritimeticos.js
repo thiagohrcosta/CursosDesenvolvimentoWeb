b15f33a8f939e93435c317352fdce74524064f68
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2020-01-09 22:48:17 -0300

    Curso Web Moderno JavaScript: Operadores

---
+++ javascript/Curso-Web-Moderno/Fundamentos/aritimeticos.js
@@ -0,0 +1,15 @@
+const [a, b, c, d] = [3, 5, 1, 15]
+
+const soma = a + b + c + d
+
+console.log(soma)
+
+const subtracao = b - d
+const mutliplicacao = a * b
+const divisao = d / a
+const modulo = a % 2
+
+console.log(subtracao)
+console.log(mutliplicacao)
+console.log(divisao)
+console.log(modulo)
