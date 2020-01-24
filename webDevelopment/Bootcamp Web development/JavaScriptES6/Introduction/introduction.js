d92b6bbc3651e22d4a40bcc4b8b1af9bda77c7fc
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2020-01-24 16:50:27 -0300

    Web Development BootCamp: JavaScript ES6 introduction

---
+++ webDevelopment/Bootcamp Web development/JavaScriptES6/Introduction/introduction.js
@@ -0,0 +1,29 @@
+alert('Hello');
+
+// Numbers
+
+// There is no special codes, just type the code
+
+1;
+
+12 + 34
+
+// Boolean - True our False
+
+prompt("What is your name?");
+
+// JavaScript variables
+
+var myName = "Thiago";
+
+
+var youName = prompt("What is your name?");
+
+alert ("My name is" + myName + ", welcome to my course" + yourName + "");
+
+var gameLevel = 1;
+gameLevel = 2;
+gameLevel = 3;
+alert(gameLevel)
+
+
