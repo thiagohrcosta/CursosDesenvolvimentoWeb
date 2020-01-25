58adcf1ecd1e8fceca161b1ff2cb722f1730362a
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2020-01-25 11:50:28 -0300

    Web Development BootCamp: JavaScript ES6 - Twitter count

---
+++ webDevelopment/Bootcamp Web development/JavaScriptES6/Introduction/strings.js
@@ -0,0 +1,11 @@
+
+// a + b
+
+alert("Hello" + " " + "World"); 
+
+var message = "Hello";
+var name = "Thiago";
+
+// write your code below this line:
+
+alert(message + " there, " + name);
---
+++ webDevelopment/Bootcamp Web development/JavaScriptES6/Introduction/twitterLimit.js
@@ -0,0 +1,6 @@
+// Little Twitter project
+
+var tweet = prompt("Compose your tweet:");
+var tweetCount = tweet.length;
+alert("You have writter " + tweetCount + " character, you have " + (148 - tweetCount) + " characters remaining.");
+
