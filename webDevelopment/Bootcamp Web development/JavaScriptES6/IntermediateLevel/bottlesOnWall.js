35f8b4826d0ca88fe0042bab350317c193bb87f6
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2020-01-25 18:52:24 -0300

    Web Development BootCamp: JavaScript ES6 - using FOR Loop for Bottle on the Wall song

--- webDevelopment/Bootcamp Web development/JavaScriptES6/IntermediateLevel/bottlesOnWall.js
+++ webDevelopment/Bootcamp Web development/JavaScriptES6/IntermediateLevel/bottlesOnWall.js
@@ -12,3 +12,30 @@ while (numberOfBottles > 0) {
     console.log(numberOfBottles + " " + bottleWord + " of beer on the wall.");
 
 }
+
+/* using FOR LOOPS
+
+for (i = 0; i < 2; i++){
+    do something
+}
+
+for (var i = 1; i < 2; i++){
+    do something
+}
+*/
+
+console.log(" Second option ");
+
+var numberOfBottles = 99
+for (var count = 100; count > 1; count--) {
+    var bottleWord = "bottle";
+    if (numberOfBottles === 1) {
+        bottleWord = "bottles";
+    } 
+    console.log(numberOfBottles + " " + bottleWord + " of beer on the wall");
+    console.log(numberOfBottles + " " + bottleWord + " of beer,");
+    console.log("Take one down, pass it around,");
+	numberOfBottles--;
+    console.log(numberOfBottles + " " + bottleWord + " of beer on the wall.");
+
+}
