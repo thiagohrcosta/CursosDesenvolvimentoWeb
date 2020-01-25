5fa43a6a30421db41146f959f37e26c720e9ea0c
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2020-01-25 18:10:48 -0300

    Web Development BootCamp: JavaScript ES6 - Array

---
+++ webDevelopment/Bootcamp Web development/JavaScriptES6/IntermediateLevel/arrays.js
@@ -0,0 +1,10 @@
+
+var guestList = ["Karev", "Grey", "O´Malley", "Steves", "Yang"]
+console.log(guestList);
+console.log(guestList.length);
+
+console.log(guestList[0]); // Karev 
+
+var guestName = prompt("What is your name?");
+
+guestList.includes(guestName)
