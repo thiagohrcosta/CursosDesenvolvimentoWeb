93c3973918766e6de411ccf4bc2d9358ca6f3c29
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2020-01-25 13:58:48 -0300

    Web Development BootCamp: JavaScript ES6 - creating some functions

---
+++ webDevelopment/Bootcamp Web development/JavaScriptES6/Introduction/functions2.js
@@ -0,0 +1,62 @@
+/* 
+function getMilk(bottles) {   
+    console.log("leaveHouse");
+    console.log("moveRight");
+    console.log("moveRight");
+    console.log("moveUp");
+    console.log("moveUp");
+    console.log("moveUp");
+    console.log("moveUp");
+    console.log("moveRight");
+    console.log("moveRight");
+    console.log("moveLeft");
+    console.log("moveLeft");
+    console.log("moveDown");
+    console.log("moveDown");
+    console.log("moveDown");
+    console.log("moveDown");
+    console.log("moveLeft");
+    console.log("moveLeft");
+    console.log("enterHouse");
+  }
+
+
+  function getMilk(bottles){
+      var cost = bottles * 1.5;
+      // Do something with cost
+  }
+
+  getMil(2);
+
+  ============================== */
+
+
+  function getMilk(money) {   
+    console.log("leaveHouse");
+    console.log("moveRight");
+    console.log("moveRight");
+    console.log("moveUp");
+    console.log("moveUp");
+    console.log("moveUp");
+    console.log("moveUp");
+    console.log("moveRight");
+    console.log("moveRight");
+
+    var numberOfBottles = Math.floor(money / 1.5);
+
+    console.log("buy " + numberOfBottles + " bottles of milk");
+
+    console.log("moveLeft");
+    console.log("moveLeft");
+    console.log("moveDown");
+    console.log("moveDown");
+    console.log("moveDown");
+    console.log("moveDown");
+    console.log("moveLeft");
+    console.log("moveLeft");
+    console.log("enterHouse");
+  }
+
+
+  getMilk(5); // $ 1.5 each
+
