9987185775c1bc61606514f1f43d0fd9a0ce65fc
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2020-01-26 13:42:11 -0300

    Web Development BootCamp: JavaScritp ES6 - Using DOM part 4

--- webDevelopment/Bootcamp Web development/DOM/index.js
+++ webDevelopment/Bootcamp Web development/DOM/index.js
@@ -1,1 +1,14 @@
 // document.querySelector("h1").innerHTML = "Good Bye";
+
+document.getElementsByTagName("li")[2].style.color = "red";
+
+document.getElementsByClassName("btn")[0].style.color = "blue";
+
+document.getElementById("title").innerHTML = " Good Bye";
+
+document.querySelector("#title");
+document.querySelector(".btn");
+
+document.querySelector("li a").innerHTML = " Google´s site";
+
+
