12b82d81857ca1ade07e843acf955e526556cba1
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2020-01-26 14:55:47 -0300

    Web Development BootCamp: JavaScript ES6 - Dicee Challenge - adding first dice

--- webDevelopment/Bootcamp Web development/DiceeChallenge/js/index.js
+++ webDevelopment/Bootcamp Web development/DiceeChallenge/js/index.js
@@ -1,1 +1,16 @@
 
+var randomNumber1 = Math.random()
+randomNumber1 = randomNumber1 * 6
+randomNumber1 = Math.floor(randomNumber1) + 1;
+
+//dice1.png - dice6.png
+var randomDiceImg = "dice" + randomNumber1 + ".png" 
+
+//images/dice1.png - images/dice6.png
+var randomImageSource = "images/" + randomDiceImg;
+
+var img1 = document.querySelectorAll("img")[0];
+
+img1.setAttribute("src", randomImageSource);
+
+
