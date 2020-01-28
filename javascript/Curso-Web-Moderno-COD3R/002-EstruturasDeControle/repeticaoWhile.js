353968c86e2996f2d3d7fc744ba72e6b2bc897f7
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2020-01-28 15:24:11 -0300

    Curso Web Moderno JavaScript: Estrutura de repetição WHILE

---
+++ javascript/Curso-Web-Moderno-COD3R/002-EstruturasDeControle/repeticaoWhile.js
@@ -0,0 +1,16 @@
+// Estrutura de repetição WHILE
+// quantidade indeterminada de repetições
+
+function getInteiroAleatorioEntre(min, max){
+    const valor = Math.random() * (max - min) + min
+    return Math.floor(valor)
+}
+
+let opcao = 0
+
+while(opcao != -1){
+    opcao = getInteiroAleatorioEntre(-1, 10)
+    console.log(`Opção escolhida foi ${opcao}.`)
+}
+
+console.log('Até a próxima.')
