b60cb6adb33269db5d5faf2dc8e6878eaeea6f87
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2020-01-30 21:58:48 -0300

    Curso Web Moderno JavaScript: This e Bind correção #2

--- javascript/Curso-Web-Moderno-COD3R/003-Função/thisEBind2.js
+++ javascript/Curso-Web-Moderno-COD3R/003-Função/thisEBind2.js
@@ -2,9 +2,8 @@ function Pessoa(){
     this.idade = 0
 
     const self = this
-
     setInterval(function(){
-        this.idade++
+        self.idade++
         console.log(self.idade)
     }/* bind(this) */, 1000)
 }
