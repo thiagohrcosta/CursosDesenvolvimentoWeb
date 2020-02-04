c6fa5ff4fc28dc25abaee8976c00b2dab0cddf03
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2020-02-03 22:21:18 -0300

    Curso Web Moderno JavaScript: Programação OO - Abstração

--- javascript/Curso-Web-Moderno-COD3R/004-Objeto/orientacaoAObjetos.js
+++ javascript/Curso-Web-Moderno-COD3R/004-Objeto/orientacaoAObjetos.js
@@ -23,3 +23,16 @@ objeto = {
 // Invoca-se o processamento a partir do dado. Há foco no objeto.
 objeto.processamento()
 
+// Princípios importantes:
+/* 1. ABSTRAÇÃO:
+*  Como pegar um objeto do mundo real e abstraí-lo para entendê-lo como
+*  um conjunto de códigos. Por exemplo, faz sentido adicionar dentro do 
+*  sistema do DETRAN a opção acelerar só por lidar com veículos? Certamente
+*  que não. 
+*  O carro é composto por parte mecânica real que é irrelevante para a sua
+*  transformação do objeto CARRO em um sistema. Neste caso do DETRAN, a placa
+*  o RENAVAN, quem é o dono, se há mais de um condutor. 
+*  O mesmo objeto pode ser abstraído de forma diferente, o carro para o DETRAN
+*  pode ter necessidades diferentes de uma oficina mecânica
+*
+*/
