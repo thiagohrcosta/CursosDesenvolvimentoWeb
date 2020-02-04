d1fb073a9a0f141869aebe701f1edd775f0cfcbe
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2020-02-03 22:13:11 -0300

    Curso Web Moderno JavaScript: Programação Orientada à Objetos - Introdução

---
+++ javascript/Curso-Web-Moderno-COD3R/004-Objeto/orientacaoAObjetos.js
@@ -0,0 +1,12 @@
+// CÓDIGO NÃO EXECUTÁVEL
+// Introdução aos conceitos da Programação Orientada à Objetos
+
+// Procedural
+// É baseado no procedimento. Há um trecho de código que recebe um nome
+// e é utilizado em vários locais. Há foco no procedimento recebendo valores 
+// de entrada, ou seja, há uma função que manipula dados.
+
+processamento(valor1, valor2, valor3)
+
+
+}
