557405120ffbb3efd2d5a61c2d26a597f5ff80fb
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2020-02-04 21:34:31 -0300

    Curso Web Moderno JavaScript: Programação OO - Criando objetos

---
+++ javascript/Curso-Web-Moderno-COD3R/004-Objeto/criandoObjetos.js
@@ -0,0 +1,23 @@
+// usando a notação literal
+
+const obj1 = {}
+console.log(obj1)
+
+// Object em JS
+console.log(typeof Object, typeof new Object)
+const obj2 = new Object
+console.log(obj2)
+
+// Funções construtoras 
+function Produto(nome, preco, desc){
+    this.nome = nome
+
+    this.getPrecoComDesconto = () => {
+        return preco * ( 1 - desc)
+    }
+}
+
+const p1 = new Produto('Caneta', 7.99, 0.15)
+const p2 = new Produto('Notebook', 2999.99, 0.25)
+
+console.log(p1.getPrecoComDesconto(), p2.getPrecoComDesconto())
