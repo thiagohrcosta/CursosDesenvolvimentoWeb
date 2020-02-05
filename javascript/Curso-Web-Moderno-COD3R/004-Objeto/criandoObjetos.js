4138531081011c608e3d9b40c012d9d2838d9b18
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2020-02-04 21:41:30 -0300

    Curso Web Moderno JavaScript: Programação OO - Criando Objetos Parte 2

--- javascript/Curso-Web-Moderno-COD3R/004-Objeto/criandoObjetos.js
+++ javascript/Curso-Web-Moderno-COD3R/004-Objeto/criandoObjetos.js
@@ -21,3 +21,31 @@ const p1 = new Produto('Caneta', 7.99, 0.15)
 const p2 = new Produto('Notebook', 2999.99, 0.25)
 
 console.log(p1.getPrecoComDesconto(), p2.getPrecoComDesconto())
+
+// Função factory
+
+function criarFuncionario(nome, salarioBase, faltas){
+    return {
+        nome, 
+        salarioBase,
+        faltas,
+        getSalario(){
+            return (salarioBase / 30) * (30 - faltas)
+        }
+    }
+}
+
+const f1 = criarFuncionario('João', 7980, 4)
+const f2 = criarFuncionario('Maria', 11400, 1)
+console.log(f1.getSalario(), f2.getSalario())
+
+// Object.create
+
+const filha = Object.create(null)
+filha.nome = 'Ana'
+console.log(filha)
+
+// Função famosa que retorna objeto...
+
+const fromJSON = JSON.parse('{"info": "Sou um JSON"}')
+console.log(fromJSON.info)
