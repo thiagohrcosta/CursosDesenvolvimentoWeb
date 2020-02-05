418e66d7b6f16ee38c7a82c945256578334b8d98
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2020-02-04 23:08:09 -0300

    Curso Web Moderno JavaScript: Programação OO - Usando Destructuring em funções

--- javascript/Curso-Web-Moderno-COD3R/004-Objeto/funcoesImportantes.js
+++ javascript/Curso-Web-Moderno-COD3R/004-Objeto/funcoesImportantes.js
@@ -8,6 +8,6 @@ console.log(Object.keys(pessoa))
 console.log(Object.values(pessoa))
 console.log(Object.entries(pessoa))
 
-Object.entries(pessoa).forEach(e => {
-    console.log(`${e[0]}: ${e[1]}`)
+Object.entries(pessoa).forEach(([chave, valor]) => {
+    console.log(`${chave}: ${valor}`)
 })
