5e72ad700b3f0b9e569fb137904b66128eb1e4b3
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2020-02-05 16:06:16 -0300

    Curso Web Moderno JavaScript: Programação OO - Herança 3

---
+++ javascript/Curso-Web-Moderno-COD3R/004-Objeto/heranca3.js
@@ -0,0 +1,20 @@
+const pai = { nome: 'Pedro', corCabelo: 'preto'}
+
+const filha1 = Object.create(pai)
+filha1.nome = 'Ana'
+console.log(filha1.corCabelo)
+
+const filha2 = Object.create(pai, {
+    nome: { value: 'Bia', writable: false, enumerable: true }
+})
+
+console.log(filha2.nome)
+filha2.nome = 'Carla'  // não será permitido alterar o nome.
+console.log(`${filha2.nome} tem cabelo ${filha2.corCabelo}`)
+
+console.log(Object.keys(filha1))
+
+for (let key in filha2){
+    filha2.hasOwnProperty(key) ?
+        console.log(key) : console.log(`Por herança: ${key}`)
+}
