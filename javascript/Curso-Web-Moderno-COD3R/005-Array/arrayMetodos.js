4ffbd6d77be9a3a1e1e22a371579ee14feb30de6
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2020-02-07 23:54:36 -0300

    Curso Web Moderno JavaScript: Array - Métodos Importantes 1

---
+++ javascript/Curso-Web-Moderno-COD3R/005-Array/arrayMetodos.js
@@ -0,0 +1,19 @@
+const pilotos = ['Vettel', 'Alonso', 'Raikkonen', 'Massa']
+
+console.log(pilotos)
+
+pilotos.pop() // Massa será removido. Remove o último elemento
+
+console.log(pilotos)
+
+pilotos.push('Verstappen') // Insere um elemento
+console.log(pilotos)
+
+pilotos.shift() // remove o primeiro elemento 
+console.log(pilotos)
+
+pilotos.unshift('Hamilton') // Adiciona um elemento na primeira posição de índice 0
+console.log(pilotos)
+
+// splice pode adicionar e remover elementos
+
