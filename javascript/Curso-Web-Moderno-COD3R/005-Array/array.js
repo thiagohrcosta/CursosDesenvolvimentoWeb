fc9476146f1c9a41fca322f82d774602b8ee0457
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2020-02-07 23:21:07 -0300

    Curso Web Moderno JavaScript: Introdução sobre Array 4

--- javascript/Curso-Web-Moderno-COD3R/005-Array/array.js
+++ javascript/Curso-Web-Moderno-COD3R/005-Array/array.js
@@ -14,13 +14,13 @@ console.log(aprovados[1])
 console.log(aprovados[2])
 console.log(aprovados[3]) // não pertence ao array. Retorna undefined.
 
-aprovados[3] = 'Paulo'
-aprovados.push('Maria')
+aprovados[3] = 'Paulo'  // método que adiciona elemento em um Array
+aprovados.push('Maria') // método CORRETO de adição de elemento em um Array
 
 console.log(aprovados.length)
 console.log(aprovados)
 
-aprovados[9] = 'João'
+aprovados[9] = 'João'  // criar um elemento no Array, cria no espaço elementos UNDEFINED
 
 console.log(aprovados)
 console.log(aprovados[8] === undefined)
@@ -34,7 +34,7 @@ console.log(aprovados[2])
 console.log(aprovados)
 
 aprovados = ['Bia', 'Carlos', 'Ana', 'Barbara']
-aprovados.splice(1, 2, 'Elemento1', 'Elemento2')
+aprovados.splice(1, 2, 'Elemento1', 'Elemento2') // Altera e adiciona elementos do Array
 console.log(aprovados)
 aprovados.splice(1,2, 'Carlos', 'Ana')
 console.log(aprovados)
