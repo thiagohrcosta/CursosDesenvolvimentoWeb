fbb2c009bc2c55eebb7814c23a3868120155ccf8
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2020-02-07 23:57:08 -0300

    Curso Web Moderno JavaScript: Array - Métodos Importantes 2

--- javascript/Curso-Web-Moderno-COD3R/005-Array/arrayMetodos.js
+++ javascript/Curso-Web-Moderno-COD3R/005-Array/arrayMetodos.js
@@ -17,3 +17,15 @@ console.log(pilotos)
 
 // splice pode adicionar e remover elementos
 
+// adicionar
+pilotos.splice(2, 0, 'Bottas', 'Massa')
+console.log(pilotos)
+
+// remover
+
+pilotos.splice(3, 1) // Massa será novamente removido
+console.log(pilotos)
+
+// slice
+const algunsPilotos1 = pilotos.slice(2) // cria novo array a partir do índice 2
+console.log(algunsPilotos1)
