1bcdc62aa26d926b36cb0f507952533ff61e613b
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2020-02-09 16:00:21 -0300

    Curso Web Moderno JavaScript: Array - Desafio Reduce

---
+++ javascript/Curso-Web-Moderno-COD3R/005-Array/reduce2.js
@@ -0,0 +1,12 @@
+const alunos = [
+    { nome: 'João', nota: 7.3, bolsista: false},
+    { nome: 'Maria', nota: 9.2, bolsista: true},
+    { nome: 'Pedro', nota: 9.8, bolsista: false},
+    { nome: 'Aana', nota: 8.7, bolsista: true} 
+]
+
+// Desafio 1: Todos os alunos são bolsistas?
+
+
+
+// Desafio 2: Algum aluno é bolsista?
