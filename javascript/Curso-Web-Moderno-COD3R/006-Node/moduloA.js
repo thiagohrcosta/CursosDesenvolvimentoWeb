79ec87a38bddc69ea894ddcebe993e8ae40c1e00
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2020-02-10 23:00:55 -0300

    Curso Web Moderno JavaScript: NODE - Criando módulos

---
+++ javascript/Curso-Web-Moderno-COD3R/006-Node/moduloA.js
@@ -0,0 +1,13 @@
+// Em NODE um arquivo representa um módulo. 
+// O módulo não deixa tudo o que se escreve disponível, ficando visível
+// apenas dentro do módulo.
+// Para torná-lo disponível será necessário exportá-lo para outro módulo.
+
+// Módulos padrões:  Commons JS  e padrão ECS2015 (import, export)
+
+this.ola = 'Bom dia pessoal'
+
+exports.bemVindo = 'Bem vindo ao NODE!'
+
+module.exports.ateLogo = 'Até o próximo exemplo'
+
