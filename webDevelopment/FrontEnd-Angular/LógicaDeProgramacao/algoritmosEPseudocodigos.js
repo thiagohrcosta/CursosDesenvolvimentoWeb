c1b9543821aad747bbea93edaa7c7d859d9c9a08
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2020-02-12 20:42:30 -0300

    Curso Front-End Angular - Algoritmos e Pseudo Códigos

---
+++ webDevelopment/FrontEnd-Angular/LógicaDeProgramacao/algoritmosEPseudocodigos.js
@@ -0,0 +1,19 @@
+// Aula 2
+/* 
+    O que é um algotitmo?
+
+    "É uma sequência de passos que resolve um dado problema"
+    Início-dia:
+        Acordar
+        Levantar da cama
+        Trocar de roupa
+        Escovar os dentes
+        Ir até a padaria
+        Tomar café
+        Escovar os dentes
+        Ir para o trabalho
+        ....
+    Fim-dia
+
+*/
+
