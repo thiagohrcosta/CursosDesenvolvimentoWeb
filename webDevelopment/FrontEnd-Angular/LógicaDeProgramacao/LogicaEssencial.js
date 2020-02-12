284ff5be07b86da97cf81556e195ea582b83518a
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2020-02-12 20:16:55 -0300

    Curso Front-End Angular - Lógica Essencial

---
+++ webDevelopment/FrontEnd-Angular/LógicaDeProgramacao/LogicaEssencial.js
@@ -0,0 +1,38 @@
+// Curso DIGITAL INNOVATION ONE
+// FRONT-END ANGULAR
+
+/* Aula 1 - Lógica essencial
+
+O que é lógica? É a coerência de raciocínio, de ideias. Que na programação
+pode se ter:
+
+    "Lógica de programação significa apenas contextualizar a lógica
+    na programação de computaores, buscando a melhor sequência de
+    ações (chamadas de algorítmos) para solucionar um problema."
+
+É importante realizar a metacognição, ou seja, "pensar como você pensa"
+
+
+*/
+
+// PROBLEMA 1
+/* Em uma loja, você deseja comprar duas blusas, cada uma custa
+   R$ 50,00, contudo, lhe é informado que comprando duas, há um 
+   desconto de 20%. 
+
+*/
+
+const blusaA = 50
+const blusaB = 50
+const desconto = 0.2
+
+const totalDeVenda = blusaA + blusaB
+
+const compra = totalDeVenda - ((totalDeVenda) * desconto)
+console.log(compra)
+
+// ABSTRAÇÃO
+/* Abstração é a habilidade de concentrar nos aspectos essenciais
+   de um contexto qualquer, ignorando características menos importantes
+   ou acidentais.
+*/
