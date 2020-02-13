598165b14705ffc1c49e9e08e4119796acc2aac5
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2020-02-12 22:05:03 -0300

    Curso Web Moderno JavaScript: NODE - Resolvendo desafio

--- javascript/Curso-Web-Moderno-COD3R/006-Node/funcionarios/funcionarios.js
+++ javascript/Curso-Web-Moderno-COD3R/006-Node/funcionarios/funcionarios.js
@@ -1,9 +1,32 @@
 const url = 'http://files.cod3r.com.br/curso-js/funcionarios.json'
 const axios = require('axios')
 
+
+// Desafio
+// Localizar funcionário conforme requisitos abaixo:
+// Gênero: Feminino, nacionalidade: Chinesa, Salário: Menor salário
+
+// Criar função para filtrar chineses
+// Criar função para filtrar mulheres chinesas
+// Criar função de agregação para pegar um único funcionário com o menor salário
+
+const chineses = f => f.pais === 'China'
+const mulheres = f => f.genero === 'F'
+const menorSalario = (func, funcAtual) => {
+    return func.salario < funcAtual.salario ? func : funcAtual
+}
+
+
+
 axios.get(url).then(response =>{
     const funcionarios = response.data
     console.log(funcionarios)
-})
 
+    const func = funcionarios
+        .filter(chineses)
+        .filter(mulheres)
+        .reduce(menorSalario)
+
+    console.log(func)
+})
 
