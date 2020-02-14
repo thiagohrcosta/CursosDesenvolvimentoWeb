aa9bca2f9293a270dd0b71087296463ff6cca5ad
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2020-02-13 21:20:21 -0300

    Curso Web Moderno JavaScript: NODE - Instância única Vs. Instância nova

---
+++ webDevelopment/FrontEnd-Angular/LógicaDeProgramacao/fluxogramaEVariaveis.js
@@ -0,0 +1,51 @@
+// Aula 3 
+
+/*
+    Fluxograma, o que é?
+    "É uma ferramente utilizada para representar graficamente o algoritmo,
+    isto é, a sequência lógica e coerente do fluxo de dado."
+
+    (Inicio) -> [Entrada] -> [Processamento]-> [Saida -> Fim
+
+    =============================================================================
+
+    O que são variáveis?
+
+    "Na programação, uma variável é um objeto (uma posição, frequentemente
+    localizada na memória) capaz de reter ou representar um valor ou expressão."
+
+        INÍCIO
+            Declara nota1: número
+            Declara nota2: número
+            Declara nota3: número
+            Declara nota4: número
+            Leia (nota1)
+            Leia (nota2)
+            Leia (nota3)
+            Leia (nota4)
+            media = (nota1 + nota 2 + nota 3 + nota 4) / 4
+            imprimir (media)
+        FIM
+
+    ============================================================================
+
+    Tipos de varíaveis
+
+    As variáveis e as constantes podem ser classificadas basicamente de quatro tipos:
+    numéricas, caracteres, alfanuméricas ou lógicas.
+
+    ============================================================================
+
+    O que é uma constante?
+
+    As constantes são valores imutáveis e não são alterados durante a vida útil
+    do programa
+
+        INÍCIO
+            DECLARA pi = 3,14     // constante
+            DECLARA raio: numero  // variável
+        FIM
+
+   
+*/
+
