c0538541745ac1a8cd781d9395cdd0fcf955614c
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2020-02-17 22:41:00 -0300

    Curso Front-End Angular - Concatenação

---
+++ webDevelopment/FrontEnd-Angular/LógicaDeProgramacao/concatenacao.js
@@ -0,0 +1,11 @@
+/* CONCATENAÇÃO
+
+    É o termo usado em computação para designar a operação
+    de unir o conteúdo de duas strings
+
+*/
+
+const nome = 'José'
+const sobrenome = 'da Silva'
+const nomeCompleto = (`${nome} ${sobrenome}`)
+console.log(nomeCompleto)
