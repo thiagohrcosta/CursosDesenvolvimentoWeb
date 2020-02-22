94e487483142c8f2e8fac196678cb0c8951ebba9
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2020-02-22 13:02:04 -0300

    Web Development BootCamp: Drum Kit Challenge - adding sounds to buttons

--- webDevelopment/Bootcamp Web development/DrumKitChallenge/index.js
+++ webDevelopment/Bootcamp Web development/DrumKitChallenge/index.js
@@ -5,9 +5,55 @@ for (var i = 0; i < numberOfDrumButtons; i++) {
     
     document.querySelectorAll(".drum")[i].addEventListener("click", function() {
         
-        // First drum 
-        var audio = new Audio("sounds/tom-1.mp3");
-        audio.play();
+        var buttonInnerHTML = this.innerHTML;
+
+        switch (buttonInnerHTML) {
+            case "w":
+                //First drum 
+                var tom1 = new Audio("sounds/tom-1.mp3");
+                tom1.play();
+                break;
+            
+            case "a":
+                // Second Drum
+                var tom2 = new Audio("sounds/tom-2.mp3");
+                tom2.play();
+                break;
+            
+            case "s":
+                // Third Drum
+                var tom3 = new Audio("sounds/tom-3.mp3")
+                tom3.play();
+                break;
+
+            case "d":
+                // Fourth
+                var tom4 = new Audio("sounds/tom-4.mp3")
+                tom4.play();
+                break;
+            
+            case "j":
+                // Snare
+                var snare = new Audio("sounds/snare.mp3")
+                snare.play();
+                break;
+            
+            case "k":
+                // Crash
+                var crash = new Audio("sounds/crash.mp3")
+                crash.play();
+                break;
+
+            case "l":
+                // kick-bass
+                var kickBass = new Audio("sounds/kick-bass.mp3")
+                kickBass.play();
+                break;
+
+                default:
+                break;
+        }
+
 
     });
 }
