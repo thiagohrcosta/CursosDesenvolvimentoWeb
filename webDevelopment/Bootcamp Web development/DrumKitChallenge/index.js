953252e25c83ef2d32c64b29e6f9c968460e790b
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2020-02-22 12:10:28 -0300

    Web Development BootCamp: Drum Kit Challenge - adding background image to buttons

--- webDevelopment/Bootcamp Web development/DrumKitChallenge/index.js
+++ webDevelopment/Bootcamp Web development/DrumKitChallenge/index.js
@@ -4,8 +4,10 @@ var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 for (var i = 0; i < numberOfDrumButtons; i++) {
     
     document.querySelectorAll(".drum")[i].addEventListener("click", function() {
-        alert("I got clicked!");
+        
+        // First drum 
+        var audio = new Audio("sounds/tom-1.mp3");
+        audio.play();
 
-    // What to do when click detected.
     });
 }
