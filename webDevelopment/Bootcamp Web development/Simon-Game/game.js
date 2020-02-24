21a02f471b1cf9f389d963e75558ce6010dbb229
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2020-02-24 12:23:18 -0300

    Web Development BootCamp: Simon Game Project - Create a new pattern

--- webDevelopment/Bootcamp Web development/Simon-Game/game.js
+++ webDevelopment/Bootcamp Web development/Simon-Game/game.js
@@ -1,1 +1,15 @@
-alert("Working")
+
+const buttonColours = ["red", "blue", "green", "yellow"];
+
+const gamePattern = [];
+
+function nextSequence(){
+    var randomNumber = Math.floor(Math.random() *4);
+
+    var randomChosenColour = buttonColours[randomNumber]
+
+    gamePattern.push(randomChosenColour)
+}
+
+
+
