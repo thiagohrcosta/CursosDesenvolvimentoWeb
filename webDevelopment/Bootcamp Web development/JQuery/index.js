4d702c622b04c251159df946541f630d63c76fdf
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2020-02-23 23:29:50 -0300

    Web Development BootCamp: JQuery - Adding event listeners

--- webDevelopment/Bootcamp Web development/JQuery/index.js
+++ webDevelopment/Bootcamp Web development/JQuery/index.js
@@ -16,3 +16,37 @@ $("h1").addClass("big-title margin-50");
 */
 
 $("a").attr("href", "http://www.yahoo.com")
+
+$("h1").click(function(){
+
+    $("h1").css("color", "red");
+
+});
+
+/*
+for (var i = 0; i < 5; i++){
+    document.querySelectorAll("button")[i].addEventListener("click", function(){
+        document.querySelector("h1").style.color = "blue"
+    });
+}
+*/
+
+$("button").click(function(){
+    $("h1").css("color", "purple");
+})
+
+$("input").keypress(function(event){
+    console.log(event.key);
+})
+
+/* CHALLENGE  
+    Change the H1 text to show what the user is typing
+*/
+
+$("input").keypress(function(event){
+    $("h1").text(event.key);
+})
+
+$("h1").on("mouseover", function(){
+    $("h1").css("color", "yellow")
+})
