f8a35287966616561eae455b4435ff3775a9659d
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2020-02-24 12:32:46 -0300

    Web Development BootCamp: Simon Game Project - Showing sequence to the user with animations and sounds

--- webDevelopment/Bootcamp Web development/Simon-Game/game.js
+++ webDevelopment/Bootcamp Web development/Simon-Game/game.js
@@ -9,6 +9,11 @@ function nextSequence(){
     var randomChosenColour = buttonColours[randomNumber]
 
     gamePattern.push(randomChosenColour)
+
+    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+
+    var audio = new Audio("sounds/" + randomChosenColour + ".mp3")
+    audio.play();
 }
 
 
