bc7eb09af27844d7604917473d40b846e7d7b549
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2020-02-23 23:19:35 -0300

    Web Development BootCamp: JQuery - Manipulating attributes

--- webDevelopment/Bootcamp Web development/JQuery/index.js
+++ webDevelopment/Bootcamp Web development/JQuery/index.js
@@ -1,14 +1,18 @@
 
-$("h1").addClass("big-title margin-50");
+$("h1").text("Bye");
 
+$("button").text("Don´t Click Me")
 
+$("button").html("<em>Stop Clicking me!</em>")
 /* 
 
 document.querySelectoAll()
 
 $("h1").removeClass("big-title");
 
+$("h1").addClass("big-title margin-50");
+
 
 */
 
-$("button");
+$("a").attr("href", "http://www.yahoo.com")
