b4799ac6cacf477ea7218c8a7aea6f304b930d72
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2020-02-24 15:02:25 -0300

    Web Development BootCamp: Simon Game Project - Finishing the project and use Beautify extension

--- webDevelopment/Bootcamp Web development/Simon-Game/game.js
+++ webDevelopment/Bootcamp Web development/Simon-Game/game.js
@@ -1,4 +1,3 @@
-
 var buttonColours = ["red", "blue", "green", "yellow"];
 
 var gamePattern = [];
@@ -8,29 +7,29 @@ var started = false;
 var level = 0;
 
 $(document).keypress(function () {
-    if (!started){
+    if (!started) {
         $("#level-title").text("Level " + level);
         nextSequence();
         started = true;
     }
 });
 
-$('.btn').click(function(){
+$(".btn").click(function () {
 
     var userChosenColour = $(this).attr("id");
     userClickedPattern.push(userChosenColour);
 
     playSound(userChosenColour);
-    animatedPress(userChosenColour);
+    animatePress(userChosenColour);
 
     checkAnswer(userClickedPattern.length - 1);
 });
 
-function checkAnswer(currentLevel){
+function checkAnswer(currentLevel) {
 
-    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]){
-        if (userClickedPattern.length === gamePattern.length){
-            setTimeout(function (){
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+        if (userClickedPattern.length === gamePattern.length) {
+            setTimeout(function () {
                 nextSequence();
             }, 1000);
         }
@@ -39,7 +38,7 @@ function checkAnswer(currentLevel){
         $("body").addClass("game-over");
         $("#level-title").text("Game Over, Press Any Key to Restart");
 
-        setTimeout(function(){
+        setTimeout(function () {
             $("body").removeClass("game-over");
         }, 200);
 
@@ -48,32 +47,31 @@ function checkAnswer(currentLevel){
 }
 
 
-function nextSequence(){
+function nextSequence() {
     userClickedPattern = [];
     level++;
-    $("#level-title").text("Level" + level);
-    var randomNumber = Math.floor(Math.random() *4);
-    var randomChosenColour = buttonColours[randomNumber]
-    gamePattern.push(randomChosenColour)
+    $("#level-title").text("Level " + level);
+    var randomNumber = Math.floor(Math.random() * 4);
+    var randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
 
     $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
     playSound(randomChosenColour);
 }
 
-function animatedPress(currentColor){
-    $("#" + currentColor),addClass("pressed");
+function animatePress(currentColor) {
+    $("#" + currentColor).addClass("pressed");
     setTimeout(function () {
         $("#" + currentColor).removeClass("pressed");
     }, 100);
 }
 
-function playSound(name){
-    var audio = new Audio("sounds/" + name + ".mp3")
-    audio.play();        
+function playSound(name) {
+    var audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
 }
 
-// Restart the game
-function startOver () {
+function startOver() {
     level = 0;
     gamePattern = [];
     started = false;
