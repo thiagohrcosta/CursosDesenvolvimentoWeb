9fd8e206269688630942e42448682e71e8ddc6c0
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2020-03-01 15:55:09 -0300

    Web Development BootCamp: Express JS with Node - BMI Calculator Project

--- webDevelopment/Bootcamp Web development/Calculator-Project/index.js
+++ webDevelopment/Bootcamp Web development/Calculator-Project/index.js
@@ -1,14 +1,63 @@
 const express = require("express");
+const bodyParser = require("body-parser")
+
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+
 
 app.get("/", function(req, res) {
     // sendfile is use to point to another file, document, or simple html page.
-    res.sendFile(__dirname + "/index.html")
-})
+    res.sendFile(__dirname + "/index.html");
+});
 
 app.post("/", function(req, res){
-    res.send("Thanks for posting that");
+
+    var num1 = Number(req.body.num1);
+    var num2 = Number(req.body.num2);
+
+    var result = num1 + num2;
+
+    res.send("The result of calculation is " + result);
+});
+
+
+// BMI Calculator method ======================
+
+app.get("/bmicalculator", function(req, res){
+    res.sendFile(__dirname + "/BMIcalculator.html");
 })
+
+app.post("/bmiCalculator", function(req, res){
+    
+    var weightBMI = parseFloat(req.body.bmiWeight);
+    var heightBMI = parseFloat(req.body.bmiHeight);
+
+    var resultBMI = weightBMI / (heightBMI * heightBMI);
+
+    res.send("Your BMI is" + resultBMI);
+/*
+    if(resultBMI < 18.5){
+        console.log("Low weight.");
+    }
+    else if (resultBMI >= 18.5 && resultBMI <= 24.9){
+        console.log("Normal weight, congratulation.");
+    }
+    else if (resultBMI >= 25 && resultBMI <= 29.9){
+        console.log("overweight.");
+    }
+    else if (resultBMI >= 30 && resultBMI <= 34.9){
+        console.log("Overweight Level 1 - ALERT!");
+    }
+    else if (resultBMI <= 35 && resultBMI <= 39.9){
+        console.log("Overweight Leve 2 - ALERT ALERT!!");
+    }
+    else{
+        console.log("Overweight Level 3 - ALERT ALERT ALERT!!!")
+    }
+*/
+
+})
+
 
 app.listen(3000, function(){
     console.log("Calculator Project running on port 3000.")
