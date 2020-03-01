a36952820a82942900fd8aae795fe493684386cc
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2020-03-01 13:25:04 -0300

    Web Development BootCamp: Express JS with Node - Using routes

--- webDevelopment/Bootcamp Web development/my-express-server/server.js
+++ webDevelopment/Bootcamp Web development/my-express-server/server.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const app = express();
 
-app.get("/", function(request, response){
-    response.send("Hello World!") 
-})
+app.get("/", function(req, res){
+    res.send("<h1>Hello, World!</h1>") 
+});
 
+app.get("/contact", function(req, res){
+    res.send("Contact me at: myemail@email.com")
+});
+
+app.get("/about", function(req, res){
+    res.send("<h1>Thiago Costa</h1><br><em>Web and System Developer</em>")
+})
 app.listen(3000, function(){
     console.log("Server started on port 3000");
 });
