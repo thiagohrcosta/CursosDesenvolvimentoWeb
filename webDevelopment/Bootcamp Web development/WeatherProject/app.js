34842d3e1411f7e931f0783c0f322ec89c250fbd
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2020-03-07 15:08:24 -0300

    Web Development BootCamp: Weather Project - Using Body parser to Parse POST request to server

--- webDevelopment/Bootcamp Web development/WeatherProject/app.js
+++ webDevelopment/Bootcamp Web development/WeatherProject/app.js
@@ -1,16 +1,23 @@
 const express = require("express");
 const https = require("https");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res){
+    res.sendFile(__dirname + "/index.html");
 
+});
+
+app.post("/", function(req, res){
+    
     // Adding custom URL to let user choose the city ===================================================================
 
-    const query = "London";
+    const query = req.body.cityName;
     const apiKey = "e908c91227bde0523b0a8a97474705cc";
-    const unit = metric;
+    const unit = "metric";
     const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "&units=" + unit;
     
     // Ending of custom URL ============================================================================================
@@ -24,15 +31,12 @@ app.get("/", function(req, res){
             const icon = weatherData.weather[0].icon;
             const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
             res.write("<p>The weather is currently " + weatherDescription + "</p>");
-            res.write("<h1>The temperature in Contagem is " + temp + "degrees Celcius.</h1>");
+            res.write("<h1>The temperature in" + query + " is " + temp + " degrees Celcius.</h1>");
             res.write("<img src=" + imageURL +">");
             res.send()
         })
     })
 })
-
-
-
 
 app.listen(3000, function(){
     console.log("Server is running on port 3000.")
