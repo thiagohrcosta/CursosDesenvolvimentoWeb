b7c84ae01b0269719b6bbedafafcdd3fcf4d8749
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2020-03-07 12:44:10 -0300

    Web Development BootCamp: Weather Project - Weather, temperature and image

--- webDevelopment/Bootcamp Web development/WeatherProject/app.js
+++ webDevelopment/Bootcamp Web development/WeatherProject/app.js
@@ -13,13 +13,14 @@ app.get("/", function(req, res){
             const weatherData = JSON.parse(data);
             const temp = weatherData.main.temp;
             const weatherDescription = weatherData.weather[0].description;
-            console.log(temp, weatherDescription);
-
+            const icon = weatherData.weather[0].icon;
+            const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+            res.write("<p>The weather is currently " + weatherDescription + "</p>");
+            res.write("<h1>The temperature in Contagem is " + temp + "degrees Celcius.</h1>");
+            res.write("<img src=" + imageURL +">");
+            res.send()
         })
     })
-    
-
-    res.send("Server is up and running.")
 })
 
 
