e2ffdc1db9c3f31b07d8aaec2b04b57d76b6d5b9
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2020-03-07 15:40:21 -0300

    Web Development BootCamp: Weather Project - Finishing HTML CSS and basic codes

--- webDevelopment/Bootcamp Web development/WeatherProject/app.js
+++ webDevelopment/Bootcamp Web development/WeatherProject/app.js
@@ -31,7 +31,7 @@ app.post("/", function(req, res){
             const icon = weatherData.weather[0].icon;
             const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
             res.write("<p>The weather is currently " + weatherDescription + "</p>");
-            res.write("<h1>The temperature in" + query + " is " + temp + " degrees Celcius.</h1>");
+            res.write("<h1>The temperature in " + query + " is " + temp + " degrees Celcius.</h1>");
             res.write("<img src=" + imageURL +">");
             res.send()
         })
