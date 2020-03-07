7882e685ac1b2233acb923cde72d46f5d0a8cadd
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2020-03-07 12:24:37 -0300

    Web Development BootCamp: Weather Project - Using JSON.parse

--- webDevelopment/Bootcamp Web development/WeatherProject/app.js
+++ webDevelopment/Bootcamp Web development/WeatherProject/app.js
@@ -6,10 +6,19 @@ const app = express();
 
 app.get("/", function(req, res){
 
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=Contagem,br&appid=e908c91227bde0523b0a8a97474705cc"
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=Contagem,br&appid=e908c91227bde0523b0a8a97474705cc&units=metric"
     https.get(url, function(response){
-        console.log(response);
+        console.log(response.statusCode);
+        response.on("data", function(data){
+            const weatherData = JSON.parse(data);
+            const object = {
+                name: "Thiago",
+                favouriteFood: "Filet a parmegiana"
+            }
+            console.log(JSON.stringify(object));
+        })
     })
+    
 
     res.send("Server is up and running.")
 })
