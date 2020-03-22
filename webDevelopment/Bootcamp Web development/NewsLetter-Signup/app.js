03727739cb1d3689a0cd849fbe7fb221f74fbefc
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2020-03-22 17:56:45 -0300

    Web Development BootCamp: Setting up the Sign Page

--- webDevelopment/Bootcamp Web development/NewsLetter-Signup/app.js
+++ webDevelopment/Bootcamp Web development/NewsLetter-Signup/app.js
@@ -6,7 +6,9 @@ const request = require("request");
 
 const app = express();
 
-
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/signup.html");
+})
 
 app.listen(3000, function(){
     console.log("Server is running on port 5000.");
