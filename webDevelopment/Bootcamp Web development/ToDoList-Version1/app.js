58ceab8557c8b3bc890b0144d62eb5cc0068cb69
thiagohrcosta <thiagohrcosta86@gmail.com>
2020-08-08 11:20:39 -0300

    Enviando mensagens para o Browser com condicionais

--- webDevelopment/Bootcamp Web development/ToDoList-Version1/app.js
+++ webDevelopment/Bootcamp Web development/ToDoList-Version1/app.js
@@ -4,7 +4,15 @@ const bodyParser = require("body-parser");
 const app = express();
 
 app.get("/", function(req, res){
-  res.send("Hello");
+  var today = new Date();
+  var currentDay = today.getDay();
+
+  if (currentDay === 6 || currentDay === 0){
+    res.send("Yeah it´s the weekend!");
+  }  
+  else{
+    res.send("Today I have to work!")
+  }
 })
 
 app.listen(3000, function(){
