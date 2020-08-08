8f14e5997b7cd761a4b27c50788afe5bc47aeb03
thiagohrcosta <thiagohrcosta86@gmail.com>
2020-08-08 11:56:25 -0300

    Creating a template using EJS

--- webDevelopment/Bootcamp Web development/ToDoList-Version1/app.js
+++ webDevelopment/Bootcamp Web development/ToDoList-Version1/app.js
@@ -1,24 +1,46 @@
-const express = require("express");
-const bodyParser = require("body-parser");
+const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.get("/", function(req, res){
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res){
+  
   var today = new Date();
   var currentDay = today.getDay();
+  var day = "";
 
-  if (currentDay === 6 || currentDay === 0){
-    res.write(
-      "<h1>Yeah it´s the weekend!</h1>"
-      );
-  }  
-  else{
-    res.write("<h1>It´s not weekend!</h1>")
-    res.write("Today I have to work!")
-    res.send();
+  switch(currentDay){
+    case 0:
+      day = 'Sunday';
+      break;
+    case 1:
+      day = 'Monday';
+      break;
+    case 2:
+      day = 'Tuesday';
+      break;
+    case 3:
+      day = 'Wednesday';
+      break;
+    case 4: 
+      day = 'Thursday';
+      break;
+    case 5:
+      day = 'Friday';
+      break;
+    case 6:
+      day = 'Saturday';
+      break;
+    default:
+      console.log('Error: Current day is equal to: ' + currentDay);
   }
-})
+
+  res.render('list', {kindOfDay: day});
+
+});
 
 app.listen(3000, function(){
-  console.log("Server started on port 3000");
+  console.log('Server started on port 3000');
 })
