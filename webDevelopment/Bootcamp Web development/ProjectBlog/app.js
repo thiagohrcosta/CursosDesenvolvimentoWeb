e52035af9a86b6c76686eec92edc85fb4d7fccbd
thiagohrcosta <thiagohrcosta86@gmail.com>
2020-08-10 09:21:49 -0300

    Inserting blog content

--- webDevelopment/Bootcamp Web development/ProjectBlog/app.js
+++ webDevelopment/Bootcamp Web development/ProjectBlog/app.js
@@ -19,6 +19,13 @@ app.get('/', function(req, res){
   res.render("home", {startingContent : homeStartingContent});
 })
 
+app.get('/about', function(req, res){
+  res.render("about", { contentAbout : aboutContent });
+})
+
+app.get('/contact', function(req, res){
+  res.render("contact", {contentContact: contactContent});
+})
 
 
 
