eb9141e3e59ac2181ef5b3c181751420389e9367
thiagohrcosta <thiagohrcosta86@gmail.com>
2020-08-11 16:36:03 -0300

    Creating a for looping to display posts

--- webDevelopment/Bootcamp Web development/ProjectBlog/app.js
+++ webDevelopment/Bootcamp Web development/ProjectBlog/app.js
@@ -10,13 +10,18 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 
 const app = express();
 
+let posts = [];
+
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get('/', function(req, res){
-  res.render("home", {startingContent : homeStartingContent});
+  res.render("home", {
+    startingContent : homeStartingContent,
+    posts: posts
+  });
 });
 
 app.get('/about', function(req, res){
@@ -30,6 +35,17 @@ app.get('/contact', function(req, res){
 app.get('/compose', function(req, res){
   res.render("compose")
 });
+
+app.post('/compose', function(req, res){
+  const post = {
+    title: req.body.postTitle,
+    profileURL: req.body.pictureUrl,
+    content: req.body.postBody
+  }
+  posts.push(post);
+
+  res.redirect("/");
+})
 
 
 
