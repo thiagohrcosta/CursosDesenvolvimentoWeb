e994f59775a393d0610c1c84d18ea4e9a4c7cf52
thiagohrcosta <thiagohrcosta86@gmail.com>
2020-09-12 13:51:06 -0300

    Finishing the post route

--- webDevelopment/Bootcamp Web development/wikiApi/app.js
+++ webDevelopment/Bootcamp Web development/wikiApi/app.js
@@ -28,7 +28,29 @@ const Article = mongoose.model("Article", articleSchema);
 
 app.get("/articles", function(req, res){
   Article.find(function(err, foundArticles){
-    console.log(foundArticles);
+    if(!err){
+      res.send(foundArticles);
+    }
+    else{
+      res.send(err);
+    }
+  });
+});
+
+app.post("/articles", function(req, res){
+
+  const newArticle = new Article({
+    title: req.body.title,
+    content: req.body.content
+  });
+
+  newArticle.save(function(err){
+    if(!err){
+      res.setDefaultEncoding("Successfully added a new article.")
+    }
+    else{
+      res.send(err);
+    }
   });
 });
 
